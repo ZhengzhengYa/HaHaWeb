ae5fd6c23842f5413a23c61d324a7061a31debf6
ZhengzhengYa <1427996372@qq.com>
2020-09-22 15:24:40 +0800

    修改表格样式

--- web-web/src/main/java/com/zz/controller/SchoolController.java
+++ web-web/src/main/java/com/zz/controller/SchoolController.java
@@ -1,11 +1,15 @@
 package com.zz.controller;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zz.model.School;
 import com.zz.model.SchoolExample;
 import com.zz.service.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
@@ -23,15 +27,17 @@ public class SchoolController {
 
     @ResponseBody
     @RequestMapping("/getListSchool")
-    public HashMap<String, Object> ListUser(){
-
+    public HashMap<String, Object> ListUser(@RequestParam int page,@RequestParam int limit){
         SchoolExample example = new SchoolExample();
         example.setOrderByClause("id ASC");
+        PageHelper.startPage(page, limit);
         List<School> schools = schoolService.find(example);
         long count = schoolService.countByExample(example);
+        PageInfo<School> schoolPageInfo = new PageInfo<>(schools,limit);
+        List<School> list = schoolPageInfo.getList();
         HashMap<String, Object> map = new HashMap<>();
         map.put("count",count);
-        map.put("data",schools);
+        map.put("data",list);
         map.put("code",0);
         return map;
     }
