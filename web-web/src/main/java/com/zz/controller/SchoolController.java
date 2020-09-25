d1f4a5f9def11226a836b141c6a3ae4d2d9b6aed
ZhengzhengYa <1427996372@qq.com>
2020-09-25 10:56:16 +0800

    新增  表格增删改功能

--- web-web/src/main/java/com/zz/controller/SchoolController.java
+++ web-web/src/main/java/com/zz/controller/SchoolController.java
@@ -8,6 +8,7 @@ import com.zz.model.SchoolExample;
 import com.zz.service.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -39,6 +40,82 @@ public class SchoolController {
         map.put("count",count);
         map.put("data",list);
         map.put("code",0);
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("/insertSchool.do")
+    public HashMap<String, Object> insert(@RequestBody HashMap<String, String> data){
+        HashMap<String, Object> map = new HashMap<>();
+        if (data != null) {
+            String seq = data.get("seq");
+            String score = data.get("score");
+            String name = data.get("name");
+            String level = data.get("level");
+            String remark = data.get("remark");
+            School school = new School();
+            school.setSeq(seq);
+            school.setScore(score);
+            school.setName(name);
+            school.setLevel(level);
+            school.setRemark(remark);
+            schoolService.insert(school);
+            map.put("msg", "添加成功！");
+            map.put("code", 0);
+        }
+        else {
+            map.put("msg", "数据异常，添加失败！");
+            map.put("code", 0);
+        }
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("/deleteSchool.do")
+    public HashMap<String, Object> insert(@RequestParam int id){
+        System.out.println(id);
+        HashMap<String, Object> map = new HashMap<>();
+        if (id > 0) {
+            schoolService.deleteByPrimaryKey(id);
+            map.put("msg","删除成功!");
+            map.put("code",0);
+        }
+        else {
+            map.put("msg","数据异常，删除失败!");
+            map.put("code",0);
+        }
+        return map;
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/editSchool.do")
+    public HashMap<String, Object> edit(@RequestBody HashMap<String, String> data){
+        HashMap<String, Object> map = new HashMap<>();
+        if (data != null) {
+            String id = data.get("id");
+            String seq = data.get("seq");
+            String score = data.get("score");
+            String name = data.get("name");
+            String level = data.get("level");
+            String remark = data.get("remark");
+            System.out.println("id="+id+" seq="+seq+" score="+score+" name="+name+" level="+level+ " remark="+remark);
+            School school = new School();
+            school.setSeq(seq);
+            school.setScore(score);
+            school.setName(name);
+            school.setLevel(level);
+            school.setRemark(remark);
+            SchoolExample example = new SchoolExample();
+            example.createCriteria().andIdEqualTo(Integer.parseInt(id));
+            schoolService.updateByExampleSelective(school,example);
+            map.put("msg", "修改成功！");
+            map.put("code", 0);
+        }
+        else {
+            map.put("msg", "数据异常，修改失败！");
+            map.put("code", 0);
+        }
         return map;
     }
 }
